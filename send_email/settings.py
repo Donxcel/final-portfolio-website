42ffc952b1398fc478dc56870d17103ca3cf6889
Donxcel <105138726+Donxcel@users.noreply.github.com>
2023-05-05 02:03:41 +0100

    updated version

--- send_email/settings.py
+++ send_email/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)=#5_z@^@n-*c%pi12ze5q24d$sqp-m6eidlk)i!b7sjxn=l0a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
